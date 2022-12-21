2e302cf670f10e240555432ee2b58e904d201b65
Prakashram27 <108420815+Prakashram27@users.noreply.github.com>
2022-12-21 16:31:04 +0530

    3 files modified, 1 added

--- src/app/basic-highlights/basic-highlights.directive.ts
+++ src/app/basic-highlights/basic-highlights.directive.ts
@@ -1,12 +1,18 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBasicHighLights]',
 })
 export class BasicHightLightDirective implements OnInit {
-  constructor(private elementRef: ElementRef) {}
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
 
   ngOnInit() {
-    this.elementRef.nativeElement.style.backgroundColor = '#544C4A';
+    // this.elementRef.nativeElement.style.backgroundColor = '#3E3053'; // This was elementRef method
+
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'background-color',
+      'yellow'
+    );
   }
 }
